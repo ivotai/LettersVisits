0e7204e9e965530c6afbd352a316941607210023
ivotai <ivotai@163.com>
2023-03-28 13:20:16 +0800

    添加材料列表。

--- app/src/main/java/com/unicorn/lettersVisits/ui/act/AddApplyAct.kt
+++ app/src/main/java/com/unicorn/lettersVisits/ui/act/AddApplyAct.kt
@@ -3,22 +3,48 @@ package com.unicorn.lettersVisits.ui.act
 
 import android.Manifest
 import android.os.Environment.getExternalStorageDirectory
+import androidx.databinding.DataBindingUtil.getBinding
+import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
 import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
 import com.afollestad.materialdialogs.files.FileFilter
 import com.afollestad.materialdialogs.files.fileChooser
-import com.blankj.utilcode.util.ToastUtils
+import com.blankj.utilcode.util.DeviceUtils.getModel
+import com.drake.brv.annotaion.DividerOrientation
+import com.drake.brv.utils.divider
+import com.drake.brv.utils.linear
+import com.drake.brv.utils.setup
 import com.drake.channel.sendEvent
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.unicorn.lettersVisits.R
 import com.unicorn.lettersVisits.app.Global
 import com.unicorn.lettersVisits.app.module.SimpleComponent
 import com.unicorn.lettersVisits.data.model.Apply
 import com.unicorn.lettersVisits.databinding.ActAddApplyBinding
+import com.unicorn.lettersVisits.databinding.ItemMaterialBinding
 import com.unicorn.lettersVisits.ui.base.BaseAct
+import io.objectbox.model.ModelProperty.addType
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
 
 
 @RuntimePermissions
 class AddApplyAct : BaseAct<ActAddApplyBinding>() {
+
+    override fun initViews() {
+        binding.apply {
+            rv.layoutManager = FlexboxLayoutManager(this@AddApplyAct)
+            rv
+                .setup {
+                addType<String>(R.layout.item_material)
+                onBind {
+                    val binding = getBinding<ItemMaterialBinding>()
+                    val item = getModel<String>()
+                    binding.tv.text = item
+                }
+            }.models = listOf("Word 格式材料", "Excel 格式材料", "PDF 格式材料", "任意格式材料")
+        }
+    }
 
     override fun initIntents() {
         binding.apply {
@@ -38,12 +64,16 @@ class AddApplyAct : BaseAct<ActAddApplyBinding>() {
 
     }
 
-    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    @NeedsPermission(
+        Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
     fun showFileDialog() {
-        val myFilter: FileFilter = { true }
-        MaterialDialog(this).show {
+        val myFilter: FileFilter = { it.isDirectory || it.extension == "docx" }
+        MaterialDialog(this, BottomSheet()).show {
             fileChooser(
-                context = context, initialDirectory = getExternalStorageDirectory(), filter = myFilter
+                context = context,
+                initialDirectory = getExternalStorageDirectory(),
+                filter = myFilter
             ) { dialog, file ->
 
                 // File selected
